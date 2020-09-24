01582e3d950976d98d6cd574fa8a39112d337e84
Maxym Volosyuk <64440035+Kuysolov@users.noreply.github.com>
2020-09-24 15:40:57 +0300

    добавляет нечто

--- js/task-7.js
+++
@@ -1,8 +0,0 @@
-const inputRangeRef = document.querySelector('#font-size-control');
-const textRef = document.querySelector('#text');
-
-inputRangeRef.addEventListener('input', onInputFontSize);
-
-function onInputFontSize(event) {
-  textRef.style.fontSize = event.currentTarget.value + 'px';
-}
